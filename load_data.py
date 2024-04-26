927e71cfdff37ba5f8015af1c2c3ee2efa9e81fc
20234582 <t.jan@student.tue.nl>
2024-04-26 13:59:31 +0200

    raw code for load_data.py added comments for load_data.py

--- load_data.py
+++ load_data.py
@@ -4,6 +4,12 @@ from pymongo import MongoClient
 import os
 from pprint import pprint
 
+"""
+The following code assumes that the data is stored in the same directory load_data.py. 
+That is, a folder called "data" is stored in the same directory as the the current file and 
+"data" contains all the json airlines files. 
+"""
+
 # establish connection to MongoDB
 client = MongoClient()
 
@@ -33,8 +39,37 @@ def load_airlines(path: str) -> None:
 def load_data():
     """ Loads data into a database collection.
     """
+    count = 1
     for json in os.listdir("data"): # iterate through all airlines files in data
+        print(count)
+        count += 1
+
         load_airlines("data/"+json)
+
+"""
+Uncomment the code below in order to load the data. Make sure that the 
+database either does not exist yet or is empty at the start to avoid duplication errors. 
+
+The counter is simply a visual to check how many json files have been inserted up to that point. 
+There are a total of 567 airline files, so the counter should run until 567 once it is finished. 
+
+Furthermore, the timer measures the total time (in sec) it took to load the data. The average
+loading time is just above 1 hour. 
+
+The error dictionary shows the amount of documents in a respective json file that were erroneous
+and thus could not be loaded into the database. 
+
+After loading all the data, make sure to comment the code again, since loading the data into 
+the database is a one-time procedure. 
+"""
+
+# # Uncomment this block of code and then run the file once!
+# start = timer()
+# load_data()
+# end = timer()
+#
+# print(end - start)
+# print(error)
 
 # close connection
 client.close()
