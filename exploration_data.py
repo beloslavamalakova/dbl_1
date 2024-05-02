d8854fa549b17207f65a1a1b97f9c8631bb0339e
20234582 <t.jan@student.tue.nl>
2024-05-02 11:22:28 +0200

    raw code for load_data.py added comments for load_data.py update README.md added exploration_data.py perform basic queries in exploration_data.py

--- exploration_data.py
+++ exploration_data.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from pprint import pprint
 
 # establish connection to MongoDB
 client = MongoClient()
@@ -15,6 +16,15 @@ non_reply_count = db.tweets_all.count_documents({"reply_count":0})
 print(non_reply_count) # result: 6508820
 # PROBLEM !!!! ALL TWEETS HAVE NON_REPLY_COUNT = 0
 
+# number of tweets that are replies ...
+# ... based on in_reply_to_status_id_str
+reply_tweets_status = db.tweets_all.count_documents({"in_reply_to_status_id_str": {"$ne": None}})
+print(reply_tweets_status) # result: 1893340
+
+# ... based on in_reply_to_user_id_str
+reply_tweets_user_id = db.tweets_all.count_documents({"in_reply_to_user_id_str": {"$ne": None}})
+print(reply_tweets_user_id) # result: 2353210
+
 # all languages
 lang_list = db.tweets_all.distinct("lang")
 print(lang_list)
@@ -24,13 +34,17 @@ lang_list = ['am', 'ar', 'bg', 'bn', 'ca', 'ckb', 'cs', 'cy', 'da', 'de', 'dv', 
              'lo', 'lt', 'lv', 'ml', 'mr', 'my', 'ne', 'nl', 'no', 'or', 'pa', 'pl', 'ps', 'pt', 'ro', 'ru',
              'sd', 'si', 'sl', 'sr', 'sv', 'ta', 'te', 'th', 'tl', 'tr', 'uk', 'und', 'ur', 'vi', 'zh']
 
-# number of tweets in english
+# number of english tweets
 en_tweets_count = db.tweets_all.count_documents({"lang":"en"})
 print(en_tweets_count) # result: 4730503
 
-# number of tweets in dutch
+# number of dutch tweets
 nl_tweets_count = db.tweets_all.count_documents({"lang":"nl"})
 print(nl_tweets_count) # result: 206641
+
+# number of undefined language tweets
+und_tweets_count = db.tweets_all.count_documents("lang":"und")
+print(und_tweets_count)
 
 # close connection
 client.close()
