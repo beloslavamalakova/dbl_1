4b03a343eb9dcb36da80f4eb27185a8be7517db7
Bernard D'hert <bernard.dhert@gmail.com>
2024-05-05 21:21:56 +0200

    Added a better counter to track the progress.

--- database_design.py
+++ database_design.py
@@ -18,10 +18,13 @@ start_time = time.time()
 # Reducing number of read and write operations
 user_operations = []
 
-#Storing all unique users so we eliminate duplicate stored users.
+# Storing all unique users so we eliminate duplicate stored users.
 unique_user_ids = set()
 
-# If statement can be removed if data is cleaned so if tweet _id not none.
+# Counter to track progress
+counter = 0
+batch_size = 1000
+
 for tweet in old_collection.find():
     tweet_id = tweet.get("id")
     if tweet_id is None:
@@ -30,7 +33,7 @@ for tweet in old_collection.find():
     user = tweet.get("user")
     user_id = None
 
-    #If statement can removed later if data is cleaned.
+    # If statement can removed later if data is cleaned.
     if user and "id" in user:
         user_id = user["id"]
 
@@ -39,24 +42,26 @@ for tweet in old_collection.find():
 
             user["_id"] = user_id
 
-            #Checking for uniuque user.
+            # Checking for unique user.
             user_operations.append(UpdateOne({"_id": user["_id"]}, {"$set": user}, upsert=True))
 
         tweet["user_id"] = user_id
 
-
-        #Removing the original tweet object from the entire thing.
+        # Removing the original tweet object from the entire thing.
         del tweet["user"]
 
-        #Maybe add removing the entire previous collection too. Can be done later, but no use in losing original database.
+    # Upsert tweet into collection
+    tweet_with_user_collection.update_one({"_id": tweet["_id"]}, {"$set": tweet}, upsert=True)
+    counter += 1
 
-    #To directly insert into collection
-    tweet_with_user_collection.insert_one(tweet)
+    # Print counter every batch_size tweets
+    if counter % batch_size == 0:
+        print(f"Processed {counter} tweets...")
 
 if user_operations:
     start_time = time.time()
 
-    #Batch operations
+    # Batch operations
     user_collection.bulk_write(user_operations)
     log_time("Batch", start_time)
 
