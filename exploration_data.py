b1a0a6602bff99be2233ed5d1cadad02e4ce52b2
20234582 <t.jan@student.tue.nl>
2024-05-26 13:00:34 +0200

    raw code for load_data.py added comments for load_data.py update README.md
    added exploration_data.py
    perform basic queries in exploration_data.py
    update load_data.py to handle duplicates and delete
    create load_data_select_att.py
    create conversations_test.py with code
    upload mock dataframe
    code to load klm conversations into csv
    update exploration_data to fit final collection

--- exploration_data.py
+++ exploration_data.py
@@ -11,7 +11,7 @@ client = MongoClient()
 
 # database and collection
 db = client["airlines"]
-tweets_att = db["tweets_att"]
+tweets_collection = db["tweets_collection"]
 
 
 # DATA EXPLORATION
@@ -31,29 +31,29 @@ non_tweet_objects = 2326
 
 # total number of unique tweet objects
 def doc_count():
-    return db.tweets_att.count_documents({})
+    return db.tweets_collection.count_documents({})
 # print(doc_count()) # result: 6094135
 
 
 # create index for created_at_datetime
-db.tweets_att.create_index([("created_at_datetime", 1)])
+db.tweets_collection.create_index([("created_at_datetime", 1)])
 
 
 # first tweet
 def first_tweet():
-    return db.tweets_att.find({}, {"created_at_datetime":1}).sort("created_at_datetime")
+    return db.tweets_collection.find({}, {"created_at_datetime":1}).sort("created_at_datetime")
 # print(first_tweet()[0]["created_at_datetime"]) # result: 2019-05-22 12:20:00
 
 
 # last tweet
 def last_tweet():
-    return db.tweets_att.find({}, {"created_at_datetime": 1}).sort([("created_at_datetime", -1)])
+    return db.tweets_collection.find({}, {"created_at_datetime": 1}).sort([("created_at_datetime", -1)])
 # print(last_tweet()[0]["created_at_datetime"]) # result: 2020-03-30 18:43:16
 
 
 # distribution over time
 def distribution_over_time():
-    dates = [date["created_at_datetime"] for date in db.tweets_att.find({}, {"created_at_datetime":1, "_id":0})]
+    dates = [date["created_at_datetime"] for date in db.tweets_collection.find({}, {"created_at_datetime":1, "_id":0})]
 
     # plot histogram
     plt.figure(figsize=(10,8))
@@ -72,12 +72,12 @@ def distribution_over_time():
 
 
 # create index for language
-db.tweets_att.create_index([("lang", 1)])
+db.tweets_collection.create_index([("lang", 1)])
 
 
 # distinct languages
 def languages():
-    return list(db.tweets_att.distinct("lang"))
+    return list(db.tweets_collection.distinct("lang"))
 # print(languages())
 
 
@@ -99,7 +99,7 @@ def frequencies_languages():
     # plot barchart
     color = ["cyan", "yellow", "blue", "orange", "grey", "green"]
 
-    languages = list(db.tweets_att.aggregate(pipeline))
+    languages = list(db.tweets_collection.aggregate(pipeline))
 
     top_5 = {lang["_id"]["language"]: lang["count"] / tweet_count for lang in languages[:5]}
     top_5["other"] = sum([lang["count"] for lang in languages[5:]]) / tweet_count
@@ -120,16 +120,16 @@ def frequencies_languages():
 
 # example undefined language tweet
 def und_tweet():
-    und_tweet = db.tweets_att.find({"lang":"und"}, {"text":1, "lang":1})[2]
+    und_tweet = db.tweets_collection.find({"lang":"und"}, {"text":1, "lang":1})[2]
     return und_tweet
 # print(und_tweet())
 
 
 # number of retweets
 def retweets():
-    quote_tweet = db.tweets_att.count_documents({"quoted_status": {"$exists": True}, "retweeted_status": {"$exists": False}})
-    retweet_tweet = db.tweets_att.count_documents({"retweeted_status": {"$exists": True}, "quoted_status": {"$exists": False}})
-    both = db.tweets_att.count_documents({"quoted_status": {"$exists": True}, "retweeted_status": {"$exists": True}})
+    quote_tweet = db.tweets_collection.count_documents({"quoted_status": {"$exists": True}, "retweeted_status": {"$exists": False}})
+    retweet_tweet = db.tweets_collection.count_documents({"retweeted_status": {"$exists": True}, "quoted_status": {"$exists": False}})
+    both = db.tweets_collection.count_documents({"quoted_status": {"$exists": True}, "retweeted_status": {"$exists": True}})
 
     labels = ["retweet tweet", "quote tweet", "both"]
     values = [retweet_tweet, quote_tweet, both]
@@ -151,13 +151,13 @@ def retweets():
 
 # number of original tweets
 def original_tweets():
-    original_tweet = db.tweets_att.count_documents({"quoted_status": {"$exists": False}, "retweeted_status": {"$exists": False}})
+    original_tweet = db.tweets_collection.count_documents({"quoted_status": {"$exists": False}, "retweeted_status": {"$exists": False}})
     return original_tweet
 # print(original_tweets()) # result: 3042450
 
 
 # create index of user mentions
-db.tweets_att.create_index("user_mentions.id")
+db.tweets_collection.create_index("user_mentions.id")
 
 # response rate of KLM
 airlines = {"KLM": 56377143, "AirFrance": 106062176, "British_Airways": 18332190, "AmericanAir": 22536055,
@@ -168,27 +168,27 @@ airlines = {"KLM": 56377143, "AirFrance": 106062176, "British_Airways": 18332190
 
 
 # create index of user id
-db.tweets_att.create_index("user.id")
+db.tweets_collection.create_index("user.id")
 
 
 def klm_mention():
-    return db.tweets_att.count_documents({"entities.user_mentions.id": airlines["KLM"]})
+    return db.tweets_collection.count_documents({"entities.user_mentions.id": airlines["KLM"]})
 # print(klm_mention) # result: 226887
 
 
 # tweets from KLM
 def klm_tweets():
-    klm_total = db.tweets_att.count_documents({"user.id": airlines["KLM"]})
-    klm_original = db.tweets_att.count_documents({"user.id": airlines["KLM"], "in_reply_to_status_id": None, "in_reply_to_user_id": None})
-    klm_reply = db.tweets_att.count_documents({"user.id": airlines["KLM"], "in_reply_to_status_id": {"$ne": None}, "in_reply_to_user_id": {"$ne": None}})
-    klm_reply_users = db.tweets_att.distinct("in_reply_to_user_id", {"user.id": airlines["KLM"],"in_reply_to_status_id": {"$ne": None}, "in_reply_to_user_id": {"$ne": None}})
+    klm_total = db.tweets_collection.count_documents({"user.id": airlines["KLM"]})
+    klm_original = db.tweets_collection.count_documents({"user.id": airlines["KLM"], "in_reply_to_status_id": None, "in_reply_to_user_id": None})
+    klm_reply = db.tweets_collection.count_documents({"user.id": airlines["KLM"], "in_reply_to_status_id": {"$ne": None}, "in_reply_to_user_id": {"$ne": None}})
+    klm_reply_users = db.tweets_collection.distinct("in_reply_to_user_id", {"user.id": airlines["KLM"],"in_reply_to_status_id": {"$ne": None}, "in_reply_to_user_id": {"$ne": None}})
 
     return klm_total, klm_original, klm_reply, (klm_reply / klm_total), (klm_total - klm_reply - klm_original), len(klm_reply_users)
 # print(klm_tweets()) # result: (34251, 155, 33993, 0.9924673732153806, 103, 17094)
 
 
 def weekly_distribution():
-    dates = [doc["created_at_datetime"] for doc in db.tweets_att.find({}, {"created_at_datetime": 1, "_id": 0})]
+    dates = [doc["created_at_datetime"] for doc in db.tweets_collection.find({}, {"created_at_datetime": 1, "_id": 0})]
 
     weekly_distribution = {'Monday': 0, 'Tuesday': 0, 'Wednesday': 0, 'Thursday': 0, 'Friday': 0, 'Saturday': 0,
                            'Sunday': 0}
