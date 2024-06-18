2bc941d2f3465b80ce3bf35d7a5352e2d18ae385
Gabriela Chavgova <g.k.chavgova@student.tue.nl>
2024-06-18 18:00:01 +0200

    added sentiment evolution plot in demo

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
+from collections import Counter
+import numpy as np
+
 
 # variables start and end date
 start_date = input('Provide a START DATE between 2019-05-22 and 2020-03-30 formatted as year-month-day: ')
@@ -19,7 +22,7 @@ mask = (df_klm_conv['created_at_datetime'] >= start_date) & (df_klm_conv['create
 df_klm_conv = df_klm_conv[mask]
 
 # dataframe of non-reply tweets directed at KLM
-df_non_reply = pd.read_csv('conversations\klm_non_reply_tweets.csv')
+df_non_reply = pd.read_csv('conversations\klm_non_reply_tweets')
 df_non_reply['created_at_datetime'] = pd.to_datetime(df_non_reply['created_at_datetime'])
 # limits entries to a specific period
 mask = (df_non_reply['created_at_datetime'] >= start_date) & (df_non_reply['created_at_datetime'] <= end_date)
@@ -30,6 +33,11 @@ df_klm_conv = df_klm_conv.set_index(['conv_starter'], drop=False)
 klm_filter = df_klm_conv[(df_klm_conv['_id'] == df_klm_conv['conv_starter']) & (df_klm_conv['user'] == klm_id)]
 df_klm_conv.drop(klm_filter.index, inplace=True)
 df_klm_conv.reset_index(drop=True, inplace=True)
+
+
+klm_sentiment_df = pd.read_csv('sentiment/klm_conv_sentiment.csv')
+mask = (klm_sentiment_df['created_at_datetime'] >= start_date) & (klm_sentiment_df['created_at_datetime'] <= end_date)
+klm_sentiment_df = klm_sentiment_df[mask]
 
 # dataframe conversation starters by clients
 df_conv_starters = df_klm_conv[(df_klm_conv['_id'] == df_klm_conv['conv_starter'])]
@@ -75,4 +83,71 @@ def distribution_languages(languages_conv_starters, languages_non_reply):
     plt.savefig("plots/distribution_languages")
     plt.show()
 
+
+def sentiment_evolution(klm_sentiment_df):
+    # Group by 'conv_starter'
+    grouped = klm_sentiment_df.groupby('conv_starter')
+
+    # Create an array of arrays for each conversation
+    conversations_array = [
+        [[row['sentiment_score'], row['sentiment'], 'airline' if row['user'] == klm_id else 'customer']
+         for _, row in group.iterrows()]
+        for _, group in grouped
+    ]
+
+    airline_conv_sentiment = []
+
+    for i in range(0, len(conversations_array)):
+        specific_conversation = conversations_array[i]
+
+        # Extract sentiment scores and user types
+        sentiment_scores = [tweet[0] for tweet in specific_conversation]
+        sentiment = [tweet[1] for tweet in specific_conversation]
+        user_types = [tweet[2] for tweet in specific_conversation]
+
+        if 'customer' not in user_types: continue
+
+        first_cust = user_types.index('customer')
+        last_cust = len(user_types) - 1 - user_types[::-1].index('customer')
+        if first_cust == last_cust: continue
+
+        airline_conv_sentiment.append(sentiment[first_cust] + '->' + sentiment[last_cust])
+
+    # Calculate counts
+    counts = Counter(airline_conv_sentiment)
+    sentiment_keys = list(counts.keys())
+    sentiment_keys.sort()
+    sorted_sentiment_count = {i: counts[i] for i in sentiment_keys}
+
+    # Define a color mapping for each key
+    color_mapping = {
+        'Negative->Positive': 'royalblue',
+        'Neutral->Positive': 'royalblue',
+        'Positive->Positive': 'royalblue',
+        'Neutral->Neutral': 'grey',
+        'Negative->Neutral': 'grey',
+        'Positive->Neutral': 'grey',
+        'Neutral->Negative': 'purple',
+        'Positive->Negative': 'purple',
+        'Negative->Negative': 'purple',
+    }
+
+    # Create a list of colors for each bar based on the color mapping
+    colors = [color_mapping[key] for key in sorted_sentiment_count.keys()]
+
+    plt.figure(figsize=(20, 5))
+    bars = plt.bar(sorted_sentiment_count.keys(), sorted_sentiment_count.values(), color=colors)
+    plt.title('KLM Bar Plot of Sentiment Evolution')
+    plt.xlabel('Sentiment Evolution')
+    plt.ylabel('Count')
+
+    # Annotating each bar with the count value
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, yval, int(yval), va='bottom')  # va: vertical alignment
+
+    plt.savefig("plots/sentiment_evolution")
+    plt.show()
+
 distribution_languages(languages_conv_starters, languages_non_reply)
+sentiment_evolution(klm_sentiment_df)
